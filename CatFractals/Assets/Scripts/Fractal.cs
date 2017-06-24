5473534d2136321ef4e8992354376056b90341c6
jthra037 <its_thrasher@live.ca>
2017-06-24 12:48:40 -0400

    Add recursive child creation

--- CatFractals/Assets/Scripts/Fractal.cs
+++ CatFractals/Assets/Scripts/Fractal.cs
@@ -15,7 +15,17 @@ public class Fractal : MonoBehaviour {
         gameObject.AddComponent<MeshRenderer>().material = material;
 
         if (depth < maxDepth) { 
-            new GameObject("Fractal Child").AddComponent<Fractal>();
+            new GameObject("Fractal Child").
+                AddComponent<Fractal>().Initialize(this);
         }
+    }
+
+    private void Initialize(Fractal parent)
+    {
+        mesh = parent.mesh;
+        material = parent.material;
+        maxDepth = parent.maxDepth;
+        depth = parent.depth + 1;
+        transform.parent = parent.transform;
     }
 }
