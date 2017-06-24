f4528ac81a95af678c77ffb60d04fc870e6e233c
jthra037 <its_thrasher@live.ca>
2017-06-24 13:00:36 -0400

    Add change of direction on fractal children

--- CatFractals/Assets/Scripts/Fractal.cs
+++ CatFractals/Assets/Scripts/Fractal.cs
@@ -9,6 +9,8 @@ public class Fractal : MonoBehaviour {
     public int maxDepth;
     private int depth;
 
+    public float childScale;
+
     private void Start()
     {
         gameObject.AddComponent<MeshFilter>().mesh = mesh;
@@ -16,16 +18,21 @@ public class Fractal : MonoBehaviour {
 
         if (depth < maxDepth) { 
             new GameObject("Fractal Child").
-                AddComponent<Fractal>().Initialize(this);
+                AddComponent<Fractal>().Initialize(this, Vector3.up);
+            new GameObject("Fractal Child").
+                AddComponent<Fractal>().Initialize(this, Vector3.right);
         }
     }
 
-    private void Initialize(Fractal parent)
+    private void Initialize(Fractal parent, Vector3 direction)
     {
         mesh = parent.mesh;
         material = parent.material;
         maxDepth = parent.maxDepth;
         depth = parent.depth + 1;
+        childScale = parent.childScale;
         transform.parent = parent.transform;
+        transform.localScale = Vector3.one * childScale;
+        transform.localPosition = direction * (0.5f + 0.5f * childScale);
     }
 }
